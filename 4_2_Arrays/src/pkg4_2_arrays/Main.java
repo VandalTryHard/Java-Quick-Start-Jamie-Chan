0ef1ab5357f0f911712f51806947b6c0f561729b
Val <gulenok.valentin@yandex.ru>
2021-10-30 00:53:09 +0600

    Java_Quick_Start_>>_Jamie_Chan
    3. ПЕРЕМЕННЫЕ И ОПЕРАТОРЫ
    4. МАССИВЫ И СТРОКИ

---
+++ 4_2_Arrays/src/pkg4_2_arrays/Main.java
@@ -0,0 +1,47 @@
+
+package pkg4_2_arrays;
+import java.util.Arrays; //4.2.1 Методы массивов
+public class Main {
+    public static void main(String[] args) {
+        int[] userAge;
+        userAge = new int[] {23, 24, 25, 26, 27, 28, 29, 30};
+        userAge[0] = 42;
+        System.out.println(userAge[0]);
+        
+        //4.2.1 Методы массивов!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        int[] arr1 = {0, 2, 4, 6, 8, 10};
+        int[] arr2 = {0, 3, 6, 9, 12, 15};
+        int[] arr3 = {0, 2, 4, 6, 8, 10};
+        
+        //equals()
+        boolean result1 = Arrays.equals(arr1, arr2);
+        boolean result2 = Arrays.equals(arr1, arr3);
+        System.out.println(result1);
+        System.out.println(result2);
+        
+        //copyOfRange()
+        int[] arrList = {0, 12, 64, -78, 35, -63, 4, -8};
+        int[] arrList1 = Arrays.copyOfRange(arrList, 1, 4);
+        System.out.println(arrList1[0]);
+        
+        //toString()
+        int[] numbers = {6, 3, 32, -95, 31, -3};
+        System.out.println(Arrays.toString(numbers));
+        
+        //sort()
+        int[] numbersSort = {93, 1, -96, 45, 22, 0, -99999, 9999};
+        Arrays.sort(numbersSort);
+        System.out.println(Arrays.toString(numbersSort));
+        
+        //binarySearch()
+        int[] numberSearch = {98, 242, -48, 0, 12, 44, 98, -23, 9};
+        Arrays.sort(numberSearch);
+        int found = Arrays.binarySearch(numberSearch, -1);
+        System.out.println(Arrays.binarySearch(numberSearch, -1)); //-3 показывает что число -1 находилось бы под индексом 3-1=2
+        
+        //4.2.2 ОПРЕДЕЛЕНИЕ ДЛИНЫ МАССИВА!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        
+        
+    }
+    
+}
