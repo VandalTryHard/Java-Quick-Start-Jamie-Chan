16b6646aed25b7f8d9bdf2af20a3b5bdcf1eb9cc
Val <gulenok.valentin@yandex.ru>
2021-11-02 16:11:24 +0600

    Java_Quick_Start_>>_Jamie_Chan
    3. ПЕРЕМЕННЫЕ И ОПЕРАТОРЫ
    4. МАССИВЫ И СТРОКИ
    5. ИНТЕРАКТИВНОСТЬ
    6. УПРАВЛЯЮЩИЕ КОМАНДЫ
    7.Объектно-ориентированное программирование
    3. VARIABLES AND OPERATORS
    4. ARRAYS AND LINES
    5. INTERACTIVITY
    6. MANAGEMENT TEAMS
    7.OOP Java

---
+++ ObjectOrientedDemo/src/objectorienteddemo/Staff.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package objectorienteddemo;
+
+/**
+ *
+ * @author Val
+ */
+public class Staff {
+    private String nameOfStaff;
+    private final int hourlyRate =30;
+    private int hoursWorked;
+    public void printMessage(){
+        System.out.println("Calculating Pay...");
+    }
+
+    public int calculatePay(){
+        printMessage();
+        int staffPay;
+        staffPay = hoursWorked * hourlyRate;
+        if (hoursWorked > 0){
+            return staffPay;
+        }else{
+            return -1;
+        }
+    }
+
+    public int calculatePay(int bonus, int allowance){
+        printMessage();
+        if (hoursWorked > 0){
+            return hoursWorked * hourlyRate + bonus + allowance;
+        }else{
+            return 0;
+        }
+    }
+
+    public void setHoursWorked(int hours){
+        if (hours > 0){
+            hoursWorked = hours;
+        }else{
+            System.out.println("Error: HoursWorked Cannot be Smaller than Zero");
+            System.out.println("Error: HoursWorked is not updated");
+        }
+    }
+
+    public int getHoursWorked(){
+        return hoursWorked;
+    }
+
+    public Staff(String name){
+        nameOfStaff = name;
+        System.out.println("n" + nameOfStaff);
+        System.out.println("--------------------------");
+    }
+
+    public Staff(String firsName, String lastName){
+        nameOfStaff = firsName + " " + lastName;
+        System.out.println("\n" + nameOfStaff);
+        System.out.println("--------------------------");
+    }
+
+}
