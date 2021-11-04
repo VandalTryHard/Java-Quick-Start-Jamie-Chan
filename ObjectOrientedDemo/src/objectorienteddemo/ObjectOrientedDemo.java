b90f59ef4fc61f0320f759eb66e0295d30cc63cc
Val <gulenok.valentin@yandex.ru>
2021-11-04 19:38:55 +0600

    Java_Quick_Start_>>_Jamie_Chan
    3. ПЕРЕМЕННЫЕ И ОПЕРАТОРЫ
    4. МАССИВЫ И СТРОКИ
    5. ИНТЕРАКТИВНОСТЬ
    6. УПРАВЛЯЮЩИЕ КОМАНДЫ
    7.Объектно-ориентированное программирование
    3. VARIABLES AND OPERATORS
    4. ARRAYS AND LINES
    5. INTERACTIVITY
    6. MANAGEMENT TEAMS
    7.OOP Java

--- ObjectOrientedDemo/src/objectorienteddemo/ObjectOrientedDemo.java
+++ ObjectOrientedDemo/src/objectorienteddemo/ObjectOrientedDemo.java
@@ -14,7 +14,6 @@ public class ObjectOrientedDemo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //TODO code application logic here
         Staff staff1 = new Staff("Peter");
         staff1.setHoursWorked(160);
         int pay = staff1.calculatePay(1000, 400);
---
+++ StaticDemo/src/staticdemo/StaticDemo.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package staticdemo; //NetBeans
+/**
+ *
+ * @author Val
+ */
+class MyClass {
+    //Нестатические поля и методы
+    public String message = "Hello World";
+    public void displayMessage(){
+        System.out.println("Message = " + message);
+    }
+
+    //Статические поля и методы
+    public static String greetings = "Good morning";
+    public static void displayGreetings(){
+        System.out.println("Greetings = " + greetings);
+    }
+}
+public class StaticDemo {
+    public static void main(String[] args){
+        MyClass sd = new MyClass();
+
+        System.out.println(sd.message);
+        sd.displayMessage();
+
+        System.out.println(MyClass.greetings);
+        MyClass.displayGreetings();
+    }
+}
